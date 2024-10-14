d98da13a35ea75c293e9c2b6e67a078ac4aa37b6
Osanyem Osadebe <osanyemo@gmail.com>
2024-10-14 07:39:26 -0400

    initial commit

---
+++ src/main/java/com/gould/slap/SlapApplication.java
@@ -0,0 +1,13 @@
+package com.gould.slap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gould/slap/SlapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gould.slap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
