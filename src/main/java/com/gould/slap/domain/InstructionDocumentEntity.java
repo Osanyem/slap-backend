fe6e5c21122740378ee4e5cea8bdafc20d6a1a95
Osanyem Osadebe <osanyemo@gmail.com>
2024-11-11 12:15:07 -0500

    fix Instruction Document Entity class

--- src/main/java/com/gould/slap/domain/InstructionDocumentEntity.java
+++ src/main/java/com/gould/slap/domain/InstructionDocumentEntity.java
@@ -6,8 +6,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//This entity is for every Instruction Documents that are uploaded by the instructor for each project
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,14 +16,14 @@ public class InstructionDocumentEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long instructionDocument_Id;
+    private Long instructionDocumentId; // Renamed to camelCase
 
     @Column(nullable = false, unique = true)
-    private String doucmentName;
+    private String documentName; // Fixed typo (doucment -> document)
 
-    private String description;
+    @Column(nullable = true)
+    private String description; // Nullable description
 
-    //Have to ensure the user that is added is a user with the role instructor
     @ManyToOne
     @JoinColumn(name = "project_id")
     private ProjectEntity projectEntity;
