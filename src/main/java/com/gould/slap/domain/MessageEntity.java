5ac71fe9322384e3ec5f876f5b9cb73e225cef1a
Osanyem Osadebe <osanyemo@gmail.com>
2024-11-11 12:25:30 -0500

    Fix Message Entity

--- src/main/java/com/gould/slap/domain/MessageEntity.java
+++ src/main/java/com/gould/slap/domain/MessageEntity.java
@@ -8,8 +8,6 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-//This entity is for the messages that are sent between users
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,7 +18,7 @@ public class MessageEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long message_Id;
+    private Long messageId;
 
     @Column(nullable = false)
     private String content;
@@ -28,13 +26,11 @@ public class MessageEntity {
     @Column(nullable = false)
     private LocalDateTime sentAt;
 
-    //Establish the relationships
     @ManyToOne
     @JoinColumn(name = "sender_id")
-    private UserEntity sender_id;
+    private UserEntity sender;
 
     @ManyToOne
     @JoinColumn(name = "receiver_id")
-    private UserEntity receiver_id;
-
+    private UserEntity receiver;
 }
